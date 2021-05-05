e7e6a50d5f2601acd4308ac9caa84170f2d7f042
LLLXY <lixinyangemil@outlook.com>
2021-05-05 22:54:08 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,50 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  let langs = await Storage.get("articleLang");
+  
+  return new Response(langs, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Max-Age": "86400"
+    }
+  });
+}
+
+//================定时任务===================
+// Cron: 0 0 * * *
+
+addEventListener('scheduled', event => {
+  event.waitUntil(
+    handleSchedule(event.scheduledTime)
+  )
+})
+
+async function handleSchedule(scheduledDate) {
+
+  let response = await fetch(`https://www.conchbrain.club/articles/config.json?${Math.random()}`);
+  let pages = await response.json();
+
+  let langs = new Array();
+
+  for(let i = 0; i < pages.length; i++) {
+    let projects = await fetch(`https://www.conchbrain.club/articles/pages/${pages[i].name}?${Math.random()}`);
+    projects = await projects.json();
+
+    for(let j = 0; j < projects.length; j++) {
+
+      let lang = projects[j].language;
+
+      if(langs.indexOf(lang) < 0) {
+        langs.push(lang);
+      }
+    }
+  }
+
+  await Storage.put("articleLang", JSON.stringify(langs));
+}
